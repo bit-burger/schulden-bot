5842163168eb01e8dbfec0c2647480d70c9b0979
Tony <65500763+bit-burger@users.noreply.github.com>
2024-03-05 17:39:47 +0100

    improve queries in calculate_debt

--- debt/calculate_debt.py
+++ debt/calculate_debt.py
@@ -7,7 +7,7 @@ def exe(sql: str, params: [any]):
 
 
 # balance of the user with each other user (but without ignored and with whitelist)
-def user_balance_query(user: RegisteredUser):
+def user_balance_cte(user: RegisteredUser):
     to_user_alias = RegisteredUser.alias("to_user")
     from_user_alias = RegisteredUser.alias("from_user")
 
@@ -20,24 +20,28 @@ def user_balance_query(user: RegisteredUser):
             .select(user_id_column, cent_amount_column)
             .join(to_user_alias, on=MoneyWrite.to_user == to_user_alias.id)
             .where(user.everyone_allowed_per_default | user_id_column.in_(whitelist_query))
-            .group_by(user_id_column))
+            .group_by(user_id_column)).cte('money_write_nets', columns=('user_id', 'cent_amount'))
+
 
 # credit_first: (should be ordered by the highest credit)
 # page_size: how big the list should be
 # page: starts by 0
 def user_balance(user: RegisteredUser, credit_first: bool, page_size: int, page: int):
-    cte = user_balance_query(user).cte('money_write_nets', columns=('user_id', 'cent_amount'))
+    cte = user_balance_cte(user)
     order_column = cte.c.cent_amount
     if not credit_first:
-        order_column = order_column.desc()
-    return RegisteredUser.select(RegisteredUser.id, cte.c.cent_amount).join(cte, on=cte.c.user_id == RegisteredUser.id).order_by(order_column).paginate(page + 1, page_size).with_cte(cte).dicts()
+        order_column = order_column
+    return (cte
+            .select_from(cte.c.user_id, cte.c.cent_amount)
+            .order_by(order_column)
+            .paginate(page + 1, page_size).dicts())
 
+
+# gives back (credit, debt)
 def user_credit_and_debt(user: RegisteredUser) -> (int, int):
-    cte = user_balance_query(user).cte('money_write_nets', columns=('user_id', 'cent_amount'))
+    cte = user_balance_cte(user)
     # select on cte instead of with_cte ?
-    credit = RegisteredUser.select(fn.sum(cte.c.cent_amount)).join(cte, on=cte.c.user_id == RegisteredUser.id).where(
-        cte.c.cent_amount > 0).with_cte(cte)
-    debt = RegisteredUser.select(fn.sum(cte.c.cent_amount)).join(cte, on=cte.c.user_id == RegisteredUser.id).where(
-        cte.c.cent_amount < 0).with_cte(cte)
+    credit = cte.select_from(fn.sum(cte.c.cent_amount)).where(cte.c.cent_amount > 0)
+    debt = cte.select_from(fn.sum(0 - cte.c.cent_amount)).where(cte.c.cent_amount < 0)
 
     return credit[0].cent_amount or 0, debt[0].cent_amount or 0  # use .tuples() ?
