dd4bf4c5d23fd1af23218d2fec1b774b74779e0c
Tony <65500763+bit-burger@users.noreply.github.com>
2024-03-06 13:25:00 +0100

    fix simple debt commands

--- commands/simple_debt.py
+++ commands/simple_debt.py
@@ -1,8 +1,7 @@
 import string
-from random import random
+import random
 from typing import Optional
 
-import discord
 from discord import app_commands
 
 from commands.utils import *
@@ -14,25 +13,21 @@ from database.permissions import can_send
 @tree.command(name='owe', description="when you owe someone",
               guild=discord.Object(id=1201588191094906890))
 @app_commands.describe(
-    euro="the amount of money that you owe in euro (will be added together with 'cent')",
-    cent="the amount of money that you owe in euro cent (will be added together with 'euro')",
+    amount="the amount of money that you owe",
     who="Who you owe the money",
     description="why you owe this money",
     attachment="an attachment for the debt (could be a photo of a receipt)",
-    # unique_identifier="a unique identifier for the debt"
 )
-@app_commands.rename(
-    # unique_identifier="unique identifier"
-)
-async def owe(i: discord.Interaction, euro: Optional[app_commands.Range[int, 0, None]],
-              cent: Optional[app_commands.Range[int, 0, None]], who: discord.Member, description: Optional[str],
+async def owe(i: discord.Interaction, amount: str, who: discord.Member, description: Optional[str],
               attachment: Optional[discord.Attachment], ):
-    if not euro and not cent:
-        return await send_success_embed(i, title="Incomplete arguments",
-                                        description="'cent' and 'euro' are both zero")
-    euro = euro or 0
-    cent = cent or 0
-    total_cent = euro * 100 + cent
+    total_cent = str_to_euro_cent(amount)
+    if who.id == i.user.id:
+        return await send_error_embed(i, title="You can't owe your self")
+    if who.bot:
+        return await send_error_embed(i, title="You can't owe a bot")
+    if not total_cent:
+        return await send_error_embed(i, title="Not a valid amount",
+                                      description=f"'**{amount}**' is not a positive sum of euro and euro cent")
     user = check_register(i)
     to_user = check_register_from_id(who.id)
     if not can_send(user, to_user):
@@ -41,14 +36,18 @@ async def owe(i: discord.Interaction, euro: Optional[app_commands.Range[int, 0, 
     if not can_send(to_user, user):
         return await send_success_embed(i, title="Cannot owe this user money",
                                         description="you have blocked or not whitelisted this user")
+    url = None
+    if attachment is not None:
+        url = attachment.url
     unique_identifier = ''.join(random.choices(string.ascii_uppercase + string.digits, k=5))
     group = MoneyWriteGroup.create(id=unique_identifier, description=description, created_by=user, type="credit",
-                                   picture=attachment.url)
-    MoneyWrite.create(group=group, cent_amount=-total_cent, from_user=user, to_user=to_user)
-    MoneyWrite.create(group=group, cent_amount=total_cent, from_user=to_user, to_user=user)
+                                   picture=url)
+    rows = [{'group': group, 'cent_amount': -total_cent, 'from_user': user, 'to_user': to_user}, {
+        'group': group, 'cent_amount': total_cent, 'from_user': to_user, 'to_user': user}]
+    MoneyWrite.insert_many(rows).execute()
 
     await send_success_embed(i, title=f"Success",
-                       description="Successfully recorded {format_euro(total_cent)} debt with <@{to_user.id}>")
+                             description=f"Successfully recorded {format_euro(total_cent)} debt with <@{to_user.id}>")
 
 # @tree.command(name='give')
 # @tree.command(name='accept')
