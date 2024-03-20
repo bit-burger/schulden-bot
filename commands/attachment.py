f7e2a9e30fe9d1eb044239693d445179a0760d02
Tony <65500763+bit-burger@users.noreply.github.com>
2024-03-20 13:23:50 +0100

    fix attachments

--- commands/attachment.py
+++ commands/attachment.py
@@ -8,13 +8,7 @@ from config import tree
 image_listener = UserListener()
 
 
-@tree.command(name='add_image', description="add image or pdf (will be converted to image) to last supporting command "
-                                            "you have been using",
-              guild=discord.Object(id=1201588191094906890))
-@app_commands.describe(
-    image="the image to add"
-)
-async def add_image(i: discord.Interaction, image: discord.Attachment):
+async def change_image(i, image: discord.Attachment):
     if not image_listener.exists_listener_for_user(i.user.id):
         return await send_error_embed(i, title="No command you are using can accept an image right now")
     if not attachment_is_image(image):
@@ -23,10 +17,20 @@ async def add_image(i: discord.Interaction, image: discord.Attachment):
     await image_listener.async_add_event(i.user.id, (i, image.url))
 
 
+@tree.command(name='add_image', description="add image or pdf (will be converted to image) to last supporting command "
+                                            "you have been using",
+              guild=discord.Object(id=1201588191094906890))
+@app_commands.describe(
+    image="the image to add"
+)
+async def add_image(i: discord.Interaction, image: discord.Attachment):
+    await change_image(i, image)
+
+
 @tree.command(name='edit_image', description="edit image or pdf of last supporting command you have been using",
               guild=discord.Object(id=1201588191094906890))
 @app_commands.describe(
     image="the new image/pdf"
 )
 async def edit_image(i, image: discord.Attachment):
-    await add_image(i, image)
+    await change_image(i, image)
