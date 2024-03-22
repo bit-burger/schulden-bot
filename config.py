d3103fe036c5d3746f771a6db06ddae980e34659
Tony <65500763+bit-burger@users.noreply.github.com>
2024-03-22 16:15:05 +0100

    add emojis

--- config.py
+++ config.py
@@ -27,11 +27,13 @@ minus_grey_emoji = f"<:minus_grey:{minus_grey_emoji_id}>"
 
 trash_can_emoji_id = config["trash_can_emoji_id"]
 trash_can_emoji = f"<:trash_can:{trash_can_emoji_id}>"
+trash_can_emoji_p = discord.PartialEmoji(name="trash_can", id=trash_can_emoji_id)
 
 help_icon_url = "https://cdn.discordapp.com/attachments/1213483785593819186/1215785651098746890/Infobox_info_icon_white.svg.png?ex=65fe039f&is=65eb8e9f&hm=f921294c968990f6593b6ad66a83d8e080aaaf73bc18625ec100e0db7b7111dd&"
 
 intents = discord.Intents.default()
 intents.message_content = True
+intents.guild_messages = True
 intents.members = True
 
 client = discord.Client(intents=intents)
